65a140c3e9b53bc25bf97f145b066748321d1597
Anda Stefania Ghita <104030121+AndaGhita@users.noreply.github.com>
2023-06-07 14:05:10 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
-#include "uart.h"
+#include "uart.h" // inkluderar egna headerfil
 
 
-int main(){
+int main(){ //deklarerar och initierar huvudfunktionen
   
-	USART2_Init();
+	USART2_Init(); // ser till att etablera UART funktion från uart.c
 	
 	//test_setup();
 	printf("Hello there from main function");
-	while(1){
+	while(1){ // loop som printar det vi efterfrågar
 	
 	}
 
