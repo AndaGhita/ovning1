fb3f7c7c07bec5e8003a2f615a133ac5f10d07b5
Anda Stefania Ghita <104030121+AndaGhita@users.noreply.github.com>
2023-06-08 10:23:32 +0200

    Update uart.c

--- uart.c
+++ uart.c
@@ -1,66 +1,66 @@
-#include "uart.h"
+#include "uart.h" // inkluderar vår lokala header och därmed dess innehåll
 
-void USART2_Init(void){
+void USART2_Init(void){ // konstrurera vår UART funktion
 
- RCC->APB1ENR  |=  0x20000; 
+ RCC->APB1ENR  |=  0x20000; // enablea clock access till UART2
 
- RCC->AHB1ENR   |=0x01;
+ RCC->AHB1ENR   |=0x01; // enablea clock-access till Port A
 
  
-GPIOA->MODER &=~0x00F0;
-GPIOA->MODER |= 0x00A0; 
+GPIOA->MODER &=~0x00F0; // sätter PA2 och 3 i reset
+GPIOA->MODER |= 0x00A0; //sätter PA2 och 3 i alternativ funktion
 
- GPIOA->AFR[0] &= ~0xFF00;
+ GPIOA->AFR[0] &= ~0xFF00;//sätt typ av alternativ funktion till 7
  GPIOA->AFR[0] |= 0x7700;
 
 
-USART2->BRR  =  0x0683;
-USART2->CR1  =  0x000C; 
-USART2->CR2  =  0x000;
-USART2->CR3  =  0x000;
-USART2->CR1  |=  0x2000; 
+USART2->BRR  =  0x0683; //hantera baud rate på utvecklingskortet
+USART2->CR1  =  0x000C; //aktiverar Tx och Rx
+USART2->CR2  =  0x000; // nollställer CR2
+USART2->CR3  =  0x000; // nollställer CR3
+USART2->CR1  |=  0x2000; // enable UART
 	
 	
 
 }
 
-int USART2_write(int ch){
+int USART2_write(int ch){ // funktion för att skriva med UART
 
 	while(!(USART2->SR & 0x0080)){}
 	USART2->DR = (ch & 0xFF);
 	
-  return ch;
+  return ch; //grundläggande funktion som skriver till registret om det är tpmt
 }
 
-int USART2_read(void){
+int USART2_read(void){ // funktion för att läsa med UART
   while(!(USART2->SR & 0x0020)){}
-	return USART2->DR;
+	return USART2->DR; // grundlegande funktion som läser ifrån registret när det ej är tomt
 }
 
-struct __FILE { int handle; };
+struct __FILE { int handle; }; // en struktur för IO-händelser
 FILE __stdin  = {0};
 FILE __stdout = {1};
 FILE __stderr = {2};
 
 
-int fgetc(FILE *f) {
+int fgetc(FILE *f) { 
     int c;
 
-    c = USART2_read();      
+    c = USART2_read();      // läs karaktär ifrån terminal
 
     if (c == '\r') {        
-        USART2_write(c);    
+        USART2_write(c);    // skriv karaktär  till terminal
         c = '\n';
     }
 
-    USART2_write(c);        
+    USART2_write(c);       //echo 
 
     return c;
 }
 
 
 
-int fputc(int c, FILE *f) {
+int fputc(int c, FILE *f) { //input funktionen 
     return USART2_write(c);  
 }
 
@@ -68,7 +68,7 @@ int fputc(int c, FILE *f) {
 int n;
 char str[80];
 		
-void test_setup(void){
+void test_setup(void){ //test funktion
 	
 	printf("please enter a number: ");
 	scanf("%d", &n);
